533e7b504d54aed4add1d2e9e4f32f6d5150f976
ddubez <ddubez40@gmail.com>
2018-09-09 15:39:54 +0200

    build: add layout buttons
    add 3 layout buttons
    add outlet et action for this buttons
    add 3 functions for set image selected

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -10,6 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+	@IBOutlet weak var button1: UIButton!
+	@IBOutlet weak var button2: UIButton!
+	@IBOutlet weak var button3: UIButton!
+
+	@IBAction func didTapButton1() {
+		layout1Choosen()
+	}
+
+	@IBAction func didTapButton2() {
+		layout2Choosen()
+	}
+
+	@IBAction func didTapButton3() {
+		layout3Choosen()
+	}
+
+	private func layout1Choosen () {
+		button1.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
+		button2.setImage(nil, for: .normal)
+		button3.setImage(nil, for: .normal)
+	}
+
+	private func layout2Choosen () {
+		button1.setImage(nil, for: .normal)
+		button2.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
+		button3.setImage(nil, for: .normal)
+	}
+	private func layout3Choosen () {
+		button1.setImage(nil, for: .normal)
+		button2.setImage(nil, for: .normal)
+		button3.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
+	}
+
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
---
+++ Instagrid/View/CentralGrid.swift
@@ -0,0 +1,19 @@
+//
+//  CentralGrid.swift
+//  Instagrid
+//
+//  Created by David DUBEZ on 09/09/2018.
+//  Copyright © 2018 David DUBEZ. All rights reserved.
+//
+
+import UIKit
+
+class CentralGrid: UIView {
+
+	enum layout {
+		case layout1, layout2, layout3
+	}
+
+	
+	
+}
