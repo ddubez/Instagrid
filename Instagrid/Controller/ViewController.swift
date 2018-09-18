f0cc19901009cca2c0a2957079745747a67d43f8
ddubez <ddubez40@gmail.com>
2018-09-18 15:17:14 +0200

    Build:  add gestion recognizer to select image
    add gesture recognizer in gridView
    add function that find the imageView selected

--- Instagrid/Controller/ViewController.swift
+++ Instagrid/Controller/ViewController.swift
@@ -8,23 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     // MARK: - OUTLET
-    //creation connexion between objects in the storyboard and variables in source code
-
     @IBOutlet weak var button1: UIButton!
     @IBOutlet weak var button2: UIButton!
     @IBOutlet weak var button3: UIButton!
     @IBOutlet weak var stackGrid: UIStackView!
 	@IBOutlet weak var swipeView: SwipeView!
+	@IBOutlet weak var gridView: UIView!
 
-    // MARK: - PROPERTIES
-    var combinedPhotos = CombinedPhotos()
+	// MARK: - PROPERTIES
+    var imageViewSet = ImageViewSet()
 
     // MARK: - ACTION
-    //creation connexion between controls in the storyboard and methods in source code
-
     @IBAction func didTapButton1() {
         layout1Choosen()
     }
@@ -38,67 +35,32 @@ class ViewController: UIViewController {
     }
 
     // MARK: - METHODS
-
-       private func layout1Choosen() {
-        // method that set the interface (button, grid) when layout 1 is chosen
-        button1.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
-        button2.setImage(nil, for: .normal)
-        button3.setImage(nil, for: .normal)
-        combinedPhotos.setGridLayout(gridLayout: .oneHightTwoLow)
-        setLayoutGrid()
-    }
-
-    private func layout2Choosen() {
-        // method that set the interface (button, grid) when layout 2 is chosen
-        button1.setImage(nil, for: .normal)
-        button2.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
-        button3.setImage(nil, for: .normal)
-        combinedPhotos.setGridLayout(gridLayout: .twoHightOneLow)
-        setLayoutGrid()
-	}
-
-    private func layout3Choosen() {
-        // method that set the interface (button, grid) when layout 3 is chosen
-        button1.setImage(nil, for: .normal)
-        button2.setImage(nil, for: .normal)
-        button3.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
-        combinedPhotos.setGridLayout(gridLayout: .twoHightTwoLow)
-        setLayoutGrid()
-    }
-
-    private func setLayoutGrid() {
-        // method that fill the grid whith images depending of the layout chosed
-        for subViews in stackGrid.arrangedSubviews {
-            // function that remove all subviews from the stackView and from the view hierarchy
-            stackGrid.removeArrangedSubview(subViews)
-            subViews.removeFromSuperview()
-        }
-        var row = 0
-        for _ in combinedPhotos.images {
-            // loop that iterate in the array of combinedPhotos,
-            // create a stackView vith the imageViews and add it to the stackGrid
-            let subStackView = UIStackView(arrangedSubviews: combinedPhotos.images[row])
-            subStackView.axis = .horizontal
-            subStackView.alignment = .fill
-            subStackView.distribution = .fillEqually
-            subStackView.spacing = 8
-            stackGrid.addArrangedSubview(subStackView)
-            row += 1
-        }
-    }
-
 	override func viewDidLoad() {
 		super.viewDidLoad()
+		// set all the 4 imageView without any photos
+		imageViewSet.imageView1 = imageViewSet.setStartImageView()
+		imageViewSet.imageView2 = imageViewSet.setStartImageView()
+		imageViewSet.imageView3 = imageViewSet.setStartImageView()
+		imageViewSet.imageView4 = imageViewSet.setStartImageView()
 
 		// Set interface in layout 1 at the start
 		layout1Choosen()
+
+		// ask the orientation at the start and set swipeView style
 		if UIApplication.shared.statusBarOrientation.isLandscape {
 			self.swipeView.style = .landscape
 		} else {
 			self.swipeView.style = .portrait
 		}
+
+		// Gesture reconizer
+		let tapGestureReconizer = UITapGestureRecognizer(target: self, action: #selector(changeImage(_:)))
+		stackGrid.addGestureRecognizer(tapGestureReconizer)
+		let swipeGestureReconizer = UITapGestureRecognizer(target: self, action: #selector(shareGrid))
+		swipeView.addGestureRecognizer(swipeGestureReconizer)
 	}
 
+	// monitor the change of orientation and set swipeView style
 	override func willTransition(
 		to newCollection: UITraitCollection,
 		with coordinator: UIViewControllerTransitionCoordinator) {
@@ -111,4 +73,92 @@ class ViewController: UIViewController {
 		})
 	}
 
+       private func layout1Choosen() {
+        // method that set the interface (button, grid) when layout 1 is chosen
+        button1.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
+        button2.setImage(nil, for: .normal)
+        button3.setImage(nil, for: .normal)
+        imageViewSet.setGridLayout(gridLayout: .oneHightTwoLow)
+        setGridViewLayout()
+    }
+
+    private func layout2Choosen() {
+        // method that set the interface (button, grid) when layout 2 is chosen
+        button1.setImage(nil, for: .normal)
+        button2.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
+        button3.setImage(nil, for: .normal)
+        imageViewSet.setGridLayout(gridLayout: .twoHightOneLow)
+        setGridViewLayout()
+	}
+
+    private func layout3Choosen() {
+        // method that set the interface (button, grid) when layout 3 is chosen
+        button1.setImage(nil, for: .normal)
+        button2.setImage(nil, for: .normal)
+        button3.setImage(#imageLiteral(resourceName: "Selected"), for: .normal)
+        imageViewSet.setGridLayout(gridLayout: .twoHightTwoLow)
+        setGridViewLayout()
+    }
+
+    private func setGridViewLayout() {
+        // method that fill the grid whith images depending of the layout chosed
+        for subViews in stackGrid.arrangedSubviews {
+            // function that remove all subviews from the stackView and from the view hierarchy
+            stackGrid.removeArrangedSubview(subViews)
+            subViews.removeFromSuperview()
+        }
+        var row = 0
+        for _ in imageViewSet.images {
+            // loop that iterate in the array of combinedPhotos,
+            // create a stackView vith the imageViews and add it to the stackGrid
+            let subStackView = UIStackView(arrangedSubviews: imageViewSet.images[row])
+            subStackView.axis = .horizontal
+            subStackView.alignment = .fill
+            subStackView.distribution = .fillEqually
+            subStackView.spacing = 8
+            stackGrid.addArrangedSubview(subStackView)
+            row += 1
+        }
+    }
+
+	@objc func changeImage(_ sender: UITapGestureRecognizer) {
+		// function that select the imageView witch is tap and change the image
+		var row = 0
+		var imageViewToChange = UIImageView()
+		for _ in imageViewSet.images {
+			var column = 0
+			for image in imageViewSet.images[row] {
+				let target = stackGrid.arrangedSubviews[row].convert(
+					stackGrid.arrangedSubviews[row].subviews[column].frame,
+					to: stackGrid)
+				if target.contains(sender.location(in: stackGrid)) {
+					print("touch")
+					print("\(target)")
+					imageViewToChange = image
+				}
+				column += 1
+			}
+			row += 1
+		}
+		// displayAlertToChangeImage()
+		let newImage = #imageLiteral(resourceName: "Selected")
+		imageViewSet.setNewImageIn(imageViewToChange, with: newImage)
+	}
+
+	func displayAlertToChangeImage() {
+		let alertController = UIAlertController(title: "Choose Image Source", message: nil, preferredStyle: .actionSheet)
+		let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+		alertController.addAction(cancelAction)
+		present(alertController, animated: true, completion: nil)
+	}
+
+	@objc func shareGrid() {
+		let capturedGridView = UIGraphicsImageRenderer(size: gridView.bounds.size)
+		let imagecapturedGridView = capturedGridView.image {_ in gridView.drawHierarchy(
+			in: gridView.bounds, afterScreenUpdates: true)
+		}
+		//let capturedGridView = gridView.snapshotView(afterScreenUpdates: false)
+		let activityController = UIActivityViewController(activityItems: [imagecapturedGridView], applicationActivities: nil)
+		present(activityController, animated: true, completion: nil)
+	}
 }
--- Instagrid/View/SwipeView.swift
+++ Instagrid/View/SwipeView.swift
@@ -9,27 +9,36 @@
 import UIKit
 
 class SwipeView: UIView {
+	//creation of subclass of UIView for manage the swipeView
+
+	// MARK: - OUTLETS
 
 	@IBOutlet private var label: UILabel!
 	@IBOutlet private var icon: UIImageView!
 
+	// MARK: - PROPERTIES
+	//creation of the text display
 	var title = "" {
 		didSet {
 			label.text = title
 		}
 	}
 
+	//creation of the style enum for set swipeView depending of the screen orientation
 	enum Style {
 		case portrait, landscape
 	}
 
+	//creation of the style
 	var style: Style = .portrait {
 		didSet {
 			setStyle()
 		}
 	}
 
+	// MARK: - METHODS
 	private func setStyle() {
+		// func that set the style of swipeView depending of style variable
 		switch style {
 		case .portrait:
 			title = "Swipe up to share"
